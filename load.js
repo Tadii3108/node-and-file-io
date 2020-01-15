6225d1f89e80bbd7e595bbd1a996981103574896
Tadii3108 <tadiwa.zingoni@umuzi.org>
2020-01-15 11:28:36 +0200

    fixed load method

--- load.js
+++ load.js
@@ -4,12 +4,9 @@ async function load(fullName) {
     let file = fullName.replace(' ', '_').toLowerCase();
 
     fs.readFile(`visitor_${file}.json`, 'utf8', (err, data) => {
-        //if (err) throw err;
         console.log(data);
     });
 }
 
-load("Lwazi");
-load("Tadiwa");
-
-module.export = {load}
+load("tadiwa");
+load("lwazi");
--- visitor.js
+++ visitor.js
@@ -11,7 +11,7 @@ class Visitor {
     }
 
     async save() {
-        let file = this.fullName.replace(' ', '_').toLowerCase();
+        let fullName = this.fullName.replace(' ', '_').toLowerCase();
 
         fs.appendFile(`visitor_${this.fullName}.json`, JSON.stringify(this, null, 4), function (err) {
             if (err) throw err;
@@ -23,6 +23,5 @@ class Visitor {
 let lwazi = new Visitor("lwazi", 26, '14/01/2020', "10:53", 'comments', "tadiwa");
 lwazi.save();
 
-let tadiwa = new Visitor("tadiwa zingoni", 21, '14/01/2020', "10:53", 'hello hi', "lwazi");
-tadiwa.save();
-
+let tadi = new Visitor("tadiwa", 21, '22/10/2019', "13:00", "the calmest ever. pleasing to work with", "marylyn");
+tadi.save();
